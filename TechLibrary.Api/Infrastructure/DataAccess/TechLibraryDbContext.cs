db35abab3865c55034b5bb5eb0c7103c58976edf
Pablo Alan <pabloxt14@gmail.com>
2026-05-20 19:10:34 -0300

    feat: create books controller and filter use-case

--- TechLibrary.Api/Infrastructure/DataAccess/TechLibraryDbContext.cs
+++ TechLibrary.Api/Infrastructure/DataAccess/TechLibraryDbContext.cs
@@ -6,7 +6,8 @@ namespace TechLibrary.Api.Infrastructure.DataAccess;
 public class TechLibraryDbContext : DbContext
 {
     public DbSet<User> Users { get; set; } // Para mostra a IDE que o DbSet não é nulo
-
+    public  DbSet<Book> Books { get; set; }
+    
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         optionsBuilder.UseSqlite("Data Source=/home/pabloalan/Documents/Studies/19_csharp/TechLibrary/TechLibraryDb.db");
---
+++ TechLibrary.Api/UseCases/Books/Filter/FilterBookUseCase.cs
@@ -0,0 +1,58 @@
+using Microsoft.EntityFrameworkCore;
+using TechLibrary.Api.Infrastructure.DataAccess;
+using TechLibrary.Communication.Requests;
+using TechLibrary.Communication.Responses;
+
+namespace TechLibrary.Api.UseCases.Books.Filter;
+
+public class FilterBookUseCase
+{
+    private const int PAGE_SIZE = 10;
+    
+    public ResponseBooksJson Execute(RequestFilterBooksJson request)
+    {
+        var dbContext = new TechLibraryDbContext();
+        
+        var skip  = (request.PageNumber - 1) * PAGE_SIZE;
+
+        var query = dbContext.Books.AsQueryable(); // Inicia a query com todos os books do banco de dados
+
+        if (!string.IsNullOrWhiteSpace(request.Title))
+        {
+            query = query.Where(book => EF.Functions.Like(book.Title, $"%{request.Title}%")); // Adiciona um filtro para o título, usando o operador LIKE do SQL
+        }
+        
+        var books = query
+            .OrderBy(book => book.Title)
+            .ThenBy(book => book.Author)
+            .Skip(skip)
+            .Take(PAGE_SIZE)
+            .ToList(); // Este comando é que será o responsável por executar a query no banco de dados
+
+        var totalCount = 0;
+
+        if (string.IsNullOrWhiteSpace(request.Title))
+        {
+            totalCount = dbContext.Books.Count();
+        }
+        else
+        {
+            totalCount = dbContext.Books.Count(book => EF.Functions.Like(book.Title, $"%{request.Title}%"));
+        }
+        
+        return new ResponseBooksJson
+        {
+            Books = books.Select(book => new ResponseBookJson
+            {
+                Id = book.Id,
+                Title = book.Title,
+                Author = book.Author
+            }).ToList(),
+            Pagination = new ResponsePaginationJson
+            {
+                PageNumber = request.PageNumber,
+                TotalCount = totalCount,
+            }
+        };
+    }
+}
