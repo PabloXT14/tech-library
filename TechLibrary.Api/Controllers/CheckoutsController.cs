7d7d3d3c55e9636b457e0eec2808df280f07591e
Pablo Alan <pabloxt14@gmail.com>
2026-06-05 18:34:37 -0300

    feat: create return book

--- TechLibrary.Api/Controllers/CheckoutsController.cs
+++ TechLibrary.Api/Controllers/CheckoutsController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using TechLibrary.Api.Services.LoggedUser;
 using TechLibrary.Api.UseCases.Checkouts;
+using TechLibrary.Communication.Responses;
 
 namespace TechLibrary.Api.Controllers;
 
@@ -12,11 +13,30 @@ public class CheckoutsController : ControllerBase
 {
     [HttpPost]
     [Route("{bookId}")]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status409Conflict)]
     public IActionResult BookCheckout(Guid bookId)
     {
-        var loggedUser = new LoggedUserService(HttpContext); 
+        var loggedUser = new LoggedUserService(HttpContext);
         
         var useCase = new RegisterBookCheckoutUseCase(loggedUser);
+        
+        useCase.Execute(bookId);
+        
+        return NoContent();
+    }
+    
+    [HttpPatch]
+    [Route("{bookId}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(typeof(ResponseErrorMessagesJson), StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(ResponseErrorMessagesJson), StatusCodes.Status409Conflict)]
+    public IActionResult ReturnBook(Guid bookId)
+    {
+        var loggedUser = new LoggedUserService(HttpContext);
+        
+        var useCase = new ReturnBookUseCase(loggedUser);
         
         useCase.Execute(bookId);
         
---
+++ TechLibrary.Api/UseCases/Checkouts/ReturnBookUseCase.cs
@@ -0,0 +1,58 @@
+using TechLibrary.Api.Domain.Entities;
+using TechLibrary.Api.Infrastructure.DataAccess;
+using TechLibrary.Api.Services.LoggedUser;
+using TechLibrary.Exception;
+
+namespace TechLibrary.Api.UseCases.Checkouts;
+
+public class ReturnBookUseCase
+{
+    private readonly LoggedUserService _loggedUserServer;
+    
+    public ReturnBookUseCase(LoggedUserService loggedUserServer)
+    {
+        _loggedUserServer = loggedUserServer;
+    }
+    
+    public void Execute(Guid bookId)
+    {
+        var dbContext = new TechLibraryDbContext();
+
+        var user = _loggedUserServer.GetLoggedUser(dbContext);
+        
+        Validate(dbContext, bookId, user);
+        
+        var checkout = dbContext.Checkouts.First(checkout => 
+            checkout.BookId == bookId &&
+            checkout.UserId == user.Id &&
+            checkout.ReturnedDate == null
+        );
+        
+        checkout.ReturnedDate = DateTime.UtcNow;
+        
+        dbContext.Checkouts.Update(checkout);
+        
+        dbContext.SaveChanges();
+    }
+
+    private void Validate(TechLibraryDbContext dbContext, Guid bookId, User loggedUser)
+    {
+        var bookExists = dbContext.Books.Any(book => book.Id == bookId);
+        
+        if  (!bookExists)
+            throw new NotFoundException("Livro não encontrado.");
+        
+        var checkout = dbContext.Checkouts.FirstOrDefault(checkout => 
+            checkout.BookId == bookId &&
+            checkout.UserId == loggedUser.Id
+        );
+        
+        if (checkout is null)
+            throw new NotFoundException("Empréstimo do livro não encontrado para o usuário logado.");
+        
+        var bookWasAlreadyReturned = checkout.ReturnedDate != null;
+        
+        if (bookWasAlreadyReturned)
+            throw new ConflictException("Livro já foi devolvido.");
+    }
+}
